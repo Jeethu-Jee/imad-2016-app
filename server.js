91255c9dd432153df9fb3a11a71d8778219dcd1d
jeethutnraj <jeethutnraj131@gmail.com>
2016-10-05 01:08:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/Article-One',function(req,res){
+app.get('/article one',function(req,res){
       res.sendFile(path.join(__dirname, 'ui', 'Article One.html'));
 });
 
-app.get('/Article-Two',function(req,res){
+app.get('/article two',function(req,res){
      res.sendFile(path.join(__dirname, 'ui', 'Article Two.html'));  
 });
 
-app.get('/Article-Three',function(req,res){
+app.get('/article three',function(req,res){
      res.sendFile(path.join(__dirname, 'ui', 'Article Three.html')); 
 });
 
