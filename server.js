948f2b1811e72b17f3aca36827fdb699a77b3609
jeethutnraj <jeethutnraj131@gmail.com>
2016-10-27 00:59:49 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ app.get('/page-two', function (req,res) {
         'page two.html'));
 });
 
-window.onload = function(){ document.getElementById("loading").style.display = "none" };
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
